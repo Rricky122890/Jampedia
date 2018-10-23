18f72e86499af1c336488736087ae41539b75d7c
Vicente <you@example.com>
2018-10-23 09:56:47 -0400

    User/Playlist update (still need views)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :playlists, to: [:index, :show]
+  resources :users, to: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
