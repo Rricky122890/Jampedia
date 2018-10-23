22b10ea5fefdc2da3188adec394391164da54dc4
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-10-23 12:32:58 -0400

    added models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :genres only: [:index, :show]
+  resources :artists only: [:index, :show, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
